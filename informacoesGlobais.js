22354dbae2e093a46fbeb10e84daa27ef1ee7ef6
vanderson244 <110059467+vandersonfoda@users.noreply.github.com>
2024-11-08 14:19:25 -0300

    Add files via upload

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -5,6 +5,15 @@ console.log(url);
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    console.log(dados);
+    const paragrafo = document.createElement("p")
+    paragrafo.classList.add("graficos-container__texto")
+    paragrafo.innerHTML = `Você sabia que o mundo tem
+${dados.total_pessoas_mundo} de pessoas eque aproximadamente 
+${dados.total_pessoas_conectadas}estão conectadas em alguma rede social e passam em média
+${dados.tempo_medio} horas conectadas.`
+const container = document.getElementById("graficos-container")
+    container.appendChild(paragrafo)
+console.log(paragrafo)
+    console.log(dados.tempo_medio);
     }
     visualizarInformacoesGlobais()
